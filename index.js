133a9b35facbb0b5000790ceb008065c4202766f
Farid Sakhyzad <farid.sakhyzad@jda.com>
2022-09-12 14:36:40 +0300

    alpha 0.0.1 WIP

--- index.js
+++ index.js
@@ -1,48 +1,80 @@
 const express = require('express');
-const app = express();
-
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
 const mongoose = require('mongoose');
 
-app.use((req, res, next) => {
-  /* console.log('\n\n\nMY MW OLOL\n\n\n');
-  console.log('======================');
-  console.log('req.method', req.method);
-  console.log('======================');
-  if (req.method === 'OPTIONS') {
-    console.log('SETTING HEADERS');
-    res.set({
-      'Access-Control-Allow-Origin': 'http://localhost:3006',
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Headers': 'x-my-header-from-front-ololo',
-    });
-  } */
-  next();
-});
-// app.use(cors({ credentials: true, origin: true }));
+const app = express();
+
+app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 
-app.options('/endpoint1', (req, res, next) => {
-  console.log('OPTIONS');
-  res.set({
-    'Access-Control-Allow-Origin': 'http://localhost:3006',
-    'Access-Control-Allow-Credentials': 'true',
-    'Access-Control-Allow-Headers': 'x-my-header-from-front-ololo',
-  });
+async function connect() {
+  await mongoose.connect('mongodb://localhost:27017/test')
+}
+
+connect().catch(err => console.error(err));
+
+app.all('/', (req, res, next) => {
+  console.log('ALL REQ *');
+  // console.log('=============');
+  // console.log('\nreq.headers', req.headers);
+  // console.log('\nreq.cookies', req.cookies);
+  // console.log('\nsession-id', req.cookies['session-id']);
+  // console.log('=============');
+
   next();
 })
 
-app.get('/endpoint1', (req, res, next) => {
-  console.log('GET REQUEST 1');
-  console.log('=============');
-  console.log('req.headers', req.headers);
-  console.log('*');
-  console.log('req.cookies', req.cookies);
-  console.log('=============');
-  console.log('\n\n');
+const kittySchema = new mongoose.Schema({
+  name: String,
+  type: String,
+});
 
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const sessionSchema = new mongoose.Schema({
+  id: String,
+  data: Object,
+  ttl: Number,
+  created: Date
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+app.get('/getSessionInfo', (req, res, next) => {
+  console.log('\n\n\nGET REQ getSessionInfo\n\n\n');
+  const sessionId = req.cookies['session-id'];
+
+  console.log('sessionId', sessionId);
+
+  Session.find({ id: sessionId }).exec((error, documents) => {
+    if (!documents.length) {
+      const userSession = new Session({
+        id: sessionId,
+        data: {},
+        ttl: 60 * 60 * 24,
+        created: +Date.now()
+      });
+
+      return userSession.save().then((data) => {
+        console.log('saved data', data);
+        return res.send({
+          message: 'HELLO. GET REQ getSessionInfo',
+          data,
+        });
+      })
+    }
+
+    console.log('documents', documents);
+
+    res.send({
+      message: 'HELLO. GET REQ getSessionInfo',
+      data: documents[0],
+    });
+  });
+});
+
+app.get('/endpoint1', (req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': 'http://localhost:3006',
     'Access-Control-Allow-Credentials': 'true',
@@ -53,74 +85,17 @@ app.get('/endpoint1', (req, res, next) => {
   res.send({ message: 'HELOOOO' });
 });
 
-async function connect() {
-  await mongoose.connect('mongodb://localhost:27017/test')
-}
-
-connect().catch(err => console.error(err));
-
-const kittySchema = new mongoose.Schema({
-  name: String,
-  type: String,
-  screenshots: [{
-    _id: false,
-    description: {type: String},
-    url: {
-      type: String,
-      required: true
-    },
-    thumbnailUrl: {type: String},
-  }],
-});
-
-function newMediaFile() {
-  const url = `https://www.trustradius.com/file${0}`;
-  return {
-    url: url,
-    description: `Description of file ${0}`,
-    thumbnail: `${url}_thum`
-  };
-}
-
-function resetModifiedPaths(model) {
-  // Sort of a hack to use internal function like this, but must do this to make
-  // mockProduct.modifiedPaths() behave like the document was saved to the db.
-  model.$__reset();
-  model.isNew = false;
-  return model;
-}
-
-const Kitten = mongoose.model('Kitten', kittySchema);
-
 const myKitten = new Kitten({
   name: 'Silence',
   type: 'default',
 });
 
-myKitten.screenshots = [ newMediaFile(), newMediaFile() ];
-
-// resetModifiedPaths(myKitten);
-
-const attrs = {
-  screenshots: [
-    Object.assign({}, myKitten.screenshots[0]),
-    Object.assign({}, myKitten.screenshots[1])
-  ]
-}
-
-attrs.screenshots[1].description = 'Modified description';
-
-myKitten.set(attrs);
-
 async function modelSave() {
   await myKitten.save();
 }
 
-modelSave().catch(err => { console.error(err) });
-
-console.log('myKitten.modifiedPaths()', myKitten.modifiedPaths());
+// modelSave().catch(err => { console.error(err) });
 
 app.listen(3007, () => {
   console.log('app listening on port 3007');
 });
-
